a509ac10ead353090851172633164f833a5e1786
camilo23401 <ca.garciav@uniandes.edu.co>
2021-04-18 22:25:29 -0500

    Primera versión

--- Caso3/src/Minado.java
+++ Caso3/src/Minado.java
@@ -1,8 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.security.*;
 
 public class Minado {
-	
+
 	@SuppressWarnings("finally")
 	public static String hash(String mensaje, String algoritmo)
 	{
@@ -13,7 +16,7 @@ public class Minado {
 			byte[] hashInicial = tipoHash.digest(mensaje.getBytes());
 			BigInteger hashComoNumero = new BigInteger(1, hashInicial);
 			hashFinal = hashComoNumero.toString(16);
-			while(hashFinal.length()< 32)
+			while(hashFinal.length()< 64)
 			{
 				hashFinal = "0"+hashFinal;
 			}
@@ -27,9 +30,39 @@ public class Minado {
 			return hashFinal;
 		}
 	}
+	public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) {
-		System.out.println(hash("GeeksForGeeks","SHA-512"));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Ingrese el algoritmo de generación de código de Hash (SHA-256 o SHA-512):");
+		// Reading data using readLine
+		String algoritmo = reader.readLine();
+		
+		System.out.println("Ingrese la cadena que representa los datos de una transacción");
+		String mensaje = reader.readLine();
+		
+		System.out.println("Ingrese el número de 0's esperado");
+		int ceros = Integer.parseInt(reader.readLine());
+
+		for(char valor = 'a'; valor <='z'; valor++ )
+		{
+			String mensajeConV = mensaje + valor;
+			String hashResultante = hash(mensajeConV, algoritmo);
+			int contadorCeros = 0;
+			int i = 0;
+			char revision = hashResultante.charAt(i);
+			while(revision=='0')
+			{
+				contadorCeros++;
+				i++;
+				revision = hashResultante.charAt(i);
+			}
+			if(contadorCeros == ceros)
+			{
+				System.out.println("El valor "+valor+" permitió cumplir la condición definida");
+				System.out.println(hashResultante);
+			}
+		}
 	}
 
 }
